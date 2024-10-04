6e9d824c88f1857d9b95aab4152ef737c44e2de8
Meljun Polillo <mnpolillo745.pbox@parsu.edu.ph>
2024-10-04 22:55:18 +0800

    Add files via upload

---
+++ Jpanel.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI;
+
+import java.awt.GridLayout;
+import javax.swing.border.Border;
+
+/**
+ *
+ * @author meljun polillo
+ */
+public class Jpanel {
+
+    void setBorder(Border createEmptyBorder) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void setLayout(GridLayout gridLayout) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
